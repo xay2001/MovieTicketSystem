a176bad8c686fe5ef381f82a7ce5f1141be849fa
xay2001 <1602357810@qq.com>
2023-04-14 16:00:37 +0800

    配置swagger文档信息

--- api/src/main/java/com/movie/api/config/SwaggerConfiguration.java
+++ api/src/main/java/com/movie/api/config/SwaggerConfiguration.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * swagger配置
@@ -23,7 +27,22 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("com.movie.api.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(new ApiInfoBuilder().title("movie电影系统 - api文档").version("v1.0").build());
+                .apiInfo(apiInfo());
+    }
+
+    //配置文档信息
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("许安源", "https://github.com/xay2001/MovieTicketingSystem", "1602357810@qq.com");
+        return new ApiInfo(
+                "爆米花电影系统 - api文档", // 标题
+                "爆米花电影系统接口文档", // 描述
+                "v1.0", // 版本
+                "https://github.com/xay2001/MovieTicketingSystem", // 组织链接
+                contact, // 联系人信息
+                "Apach 2.0 许可", // 许可
+                "许可链接", // 许可连接
+                new ArrayList<>()// 扩展
+        );
     }
 
 }
